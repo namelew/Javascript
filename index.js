f890cb1f843a6941a1b2db8558234784a0fd6f20
“namelew” <diogomaciel.cunha@gmail.com>
2021-11-14 17:00:49 -0300

    build de sum display

--- index.js
+++ index.js
@@ -2,13 +2,30 @@ let firstCard = Math.floor(Math.random() * 10) + 2
 let secondCard = Math.floor(Math.random() * 10) + 2
 
 let sum = firstCard + secondCard
+let hasBlackJack = false
+let isAlive = true
+let message = ""
+let messageEl = document.getElementById("message-el")
+let sumEl = document.querySelector("#sum-el")
 
-console.log(sum)
+function displaySum(){
+    if(sumEl.textContent != "Sum: "){
+        sumEl.textContent = "Sum: " + sum
+    } else{
+        sumEl.textContent += sum
+    }
+}
 
-if(sum < 21){
-    console.log("Do you want to draw a new card?")
-}else if(sum === 21){
-    console.log("You've got blackjack!")
-} else if(sum > 21){
-    console.log("You're out of the game!")
+function startGame(){
+    displaySum()
+    if(sum <= 20){
+        message = "Do you want to draw a new card?"
+    }else if(sum === 21){
+        message = "You've got blackjack!"
+        hasBlackJack = true
+    } else{
+        message = "You're out of the game!"
+        isAlive = false
+    }
+    messageEl.innerText = message
 }
