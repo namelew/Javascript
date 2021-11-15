7b9159679c5d3fcfa86bb94c3fb624f1ba779c93
“namelew” <diogomaciel.cunha@gmail.com>
2021-11-15 20:55:37 -0300

    creanting a li of <a>

--- index.js
+++ index.js
@@ -5,12 +5,16 @@ const ulEl = document.getElementById("ul-el")
 
 inputBtn.addEventListener("click", function (){
     myLeads.push(inputEl.value)
+    inputEl.value = ""
+    renderLeads()
 })
 
-for(let i=0; i < myLeads.length; i++){
-    //ulEl.innerHTML += "<li>"+ myLeads[i] + "</li>"
-    // same thing
-    const li = document.createElement("li")
-    li.textContent = myLeads[i]
-    ulEl.append(li)
+
+function renderLeads(){
+    let listItens = ""
+    for(let i=0; i < myLeads.length; i++){
+        //listItens += "<li>"+"<a target='_blank'href='"+myLeads[i]+"'>"+ myLeads[i] +"</a>"+ "</li>"
+        listItens += "<li>"+"<a target='_blank'href='"+myLeads[i]+"'>"+ myLeads[i] +"</a>"+ "</li>"
+    }
+    ulEl.innerHTML = listItens
 }
