5f30a57717c79abefe8736f249d01d5aa58be627
“namelew” <diogomaciel.cunha@gmail.com>
2021-11-15 20:34:41 -0300

    make things

--- challenge/index.js
+++
@@ -1,5 +0,0 @@
-let box = document.querySelector("#box")
-
-box.addEventListener("click", function(){
-    console.log("I want to open the box!")
-})
--- index.js
+++ index.js
@@ -1,5 +1,16 @@
-let inputBtn = document.getElementById("input-btn")
+let myLeads = []
+const inputEl = document.getElementById("input-el")
+const inputBtn = document.getElementById("input-btn")
+const ulEl = document.getElementById("ul-el")
 
 inputBtn.addEventListener("click", function (){
-    console.log("Button clicked")
+    myLeads.push(inputEl.value)
 })
+
+for(let i=0; i < myLeads.length; i++){
+    //ulEl.innerHTML += "<li>"+ myLeads[i] + "</li>"
+    // same thing
+    const li = document.createElement("li")
+    li.textContent = myLeads[i]
+    ulEl.append(li)
+}
