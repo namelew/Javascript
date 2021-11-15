6228496d2df584533df4a156af5fb22c3a5c56ee
“namelew” <diogomaciel.cunha@gmail.com>
2021-11-15 12:04:22 -0300

    creating the bases of the app

--- index.js
+++ index.js
@@ -1,13 +1,3 @@
-let fruit = ["apple", "orange", "apple", "apple", "orange"]
-let appleEl = document.getElementById("apple-el")
-let orangeEl = document.getElementById("orange-el")
-
-function set() {
-    for(let i = 0; i < fruit.length; i++){
-        if(fruit[i] == "apple"){
-            appleEl.textContent += fruit[i] + "\n"
-        }else{
-            orangeEl.textContent += fruit[i] + "\n"
-        }
-    }
+function saveInput() {
+    console.log("Buttton clicked")
 }
