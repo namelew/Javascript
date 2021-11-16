b1c0c1efbbc901ba4de73cdd3b7160fd9b0096f6
“namelew” <diogomaciel.cunha@gmail.com>
2021-11-16 01:28:02 -0300

    solving save current tab problem

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ deleteBtn.addEventListener("dblclick", function (){
 })
 
 tabBtn.addEventListener("click", function (){
-    chrome.tabs.query({active:yes, currentWindows: true}, function (tabs){
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
         myLeads.push(tabs[0].url)
         localStorage.setItem("myLeads", JSON.stringify(myLeads))
         render(myLeads)
