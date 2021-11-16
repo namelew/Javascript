134b58fbcdc2810b69d4d66bef50187d45cb3e94
“namelew” <diogomaciel.cunha@gmail.com>
2021-11-16 01:20:26 -0300

    integrating the tab btn with the chrome api

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ let myLeads = []
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const deleteBtn = document.getElementById("delete-btn")
+const tabBtn = document.getElementById("tab-btn")
 const ulEl = document.getElementById("ul-el")
-
 const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
 
 if(leadsFromLocalStorage){
@@ -34,4 +34,12 @@ deleteBtn.addEventListener("dblclick", function (){
     myLeads = []
     localStorage.clear()
     render(myLeads)
+})
+
+tabBtn.addEventListener("click", function (){
+    chrome.tabs.query({active:yes, currentWindows: true}, function (tabs){
+        myLeads.push(tabs[0].url)
+        localStorage.setItem("myLeads", JSON.stringify(myLeads))
+        render(myLeads)
+    })
 })
